2bb9eadca79844e70059db3684b0f58e0385f6ab
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-01-07 09:31:54 +0700

    Change Date displayed ago

--- counter-challenge/src/index.js
+++ counter-challenge/src/index.js
@@ -8,11 +8,11 @@ function App() {
   const todayDate = moment().format("D-MM-YYYY");
 
   const [step, setStep] = useState(1);
-  const [counter, setCounter] = useState(1);
+  const [count, setCount] = useState(1);
   const [date, setDate] = useState(todayDate);
 
   function changeDate() {
-    const numberOfDayChanges = step * counter;
+    const numberOfDayChanges = step * count;
     const newDate = moment()
       .add(numberOfDayChanges, "days")
       .format("D-MM-YYYY");
@@ -27,12 +27,12 @@ function App() {
     setStep((s) => s - 1);
   }
 
-  function plusCounter() {
-    setCounter((c) => c + 1);
+  function plusCount() {
+    setCount((c) => c + 1);
   }
 
-  function minusCounter() {
-    setCounter((c) => c - 1);
+  function minusCount() {
+    setCount((c) => c - 1);
   }
 
   function Step() {
@@ -45,25 +45,25 @@ function App() {
     );
   }
 
-  function Counter() {
+  function Count() {
     return (
       <div>
-        <button onClick={minusCounter}>-</button>
-        <span> Counter: {counter} </span>
-        <button onClick={plusCounter}>+</button>
+        <button onClick={minusCount}>-</button>
+        <span> Count: {count} </span>
+        <button onClick={plusCount}>+</button>
       </div>
     );
   }
 
   function Banner() {
-    const dayChanges = step * counter;
+    const dayChanges = step * count;
     changeDate();
     return (
       <>
         <span>
           {dayChanges >= 0
             ? `${dayChanges} days from now is: `
-            : `${dayChanges} days ago was: `}
+            : `${Math.abs(dayChanges)} days ago was: `}
         </span>
         <span>{date}</span>
       </>
@@ -73,7 +73,7 @@ function App() {
   return (
     <div className="App">
       <Step />
-      <Counter />
+      <Count />
       <Banner />
     </div>
   );
