27e75cf09a1effcbc4c6ea62d600f44dbb04a75b
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-01-09 14:30:24 +0700

    Add JSX component as children props

--- steps/src/App.js
+++ steps/src/App.js
@@ -58,9 +58,18 @@ function Step() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           {/*
             Do not use onClick={handlePrevious()} 
@@ -81,6 +90,15 @@ function Step() {
         </div>
       )}
     </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </p>
   );
 }
 
