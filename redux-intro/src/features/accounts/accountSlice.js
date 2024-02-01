73478f2e8c81bfda35320e4db58b03463d184742
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-01 13:16:19 -0800

    Introduce Redux Slice pattern

---
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -0,0 +1,60 @@
+const initialAccountState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+// const ACCOUNT_DEPOSIT = "account/deposit";
+
+//pure functions only, no side effects.
+//default of initialState at the beginning
+export default function accountReducer(state = initialAccountState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      //Redux recommends not throwing error, but return unchanged state
+      return state;
+  }
+}
+
+export function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount,
+      purpose,
+    },
+  };
+}
+
+export function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
