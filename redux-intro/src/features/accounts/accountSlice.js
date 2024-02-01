3037ea40f8fc9a9d78ad9e9cb8b9506f0234234c
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-01 15:10:13 -0800

    Redux middleware with Thunk

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -2,6 +2,7 @@ const initialAccountState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 // const ACCOUNT_DEPOSIT = "account/deposit";
@@ -11,7 +12,13 @@ const initialAccountState = {
 export default function accountReducer(state = initialAccountState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      const newState = {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
+      console.log(newState);
+      return newState;
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
@@ -29,14 +36,31 @@ export default function accountReducer(state = initialAccountState, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
     default:
       //Redux recommends not throwing error, but return unchanged state
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+//this has become a middleware function
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  //return a dispatch function instead. Redux know this is a side effect and sent to middleware instead
+  return async function (dispatch, getState) {
+    //Set Loading
+    dispatch({ type: "account/convertingCurrency" });
+
+    //API call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    //call the reducer
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 
 export function withdraw(amount) {
