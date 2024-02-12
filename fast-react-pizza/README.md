f7a4d19c3f8b5a4dde44b52fc1dfa99354232d91
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-11 18:32:56 -0800

    Tailwind prettier

--- fast-react-pizza/README.md
+++ fast-react-pizza/README.md
@@ -5,3 +5,19 @@ https://tailwindcss.com/docs/guides/vite
 # Preflight Tailwind style is automatically applying
 
 https://tailwindcss.com/docs/preflight
+
+# Install Tailwind CSS Intellisense pluggin for VS Code
+
+# Install Prettier extension
+
+https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+
+`npm install -D prettier prettier-plugin-tailwindcss`
+` prettier.config.js
+module.exports = {
+plugins: ['prettier-plugin-tailwindcss'],
+}`
+
+# Prettier config can be in js, json, yaml or cjs form:
+
+https://prettier.io/docs/en/configuration.html
--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,15 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import Home from './ui/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
 //https://reactrouter.com/en/6.22.0/routers/create-browser-router
 /**
@@ -22,22 +22,22 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: "cart", element: <Cart /> },
+      { path: 'cart', element: <Cart /> },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
-      { path: "/order/:orderId", element: <Order />, loader: orderLoader },
+      { path: '/order/:orderId', element: <Order />, loader: orderLoader },
     ],
   },
 ]);
