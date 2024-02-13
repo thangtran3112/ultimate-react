8c889ff9e1d411e57194dcb146b9c804eb614384
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-12 20:28:09 -0800

    css grid tailwind

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,21 +1,25 @@
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
-import Header from "./Header";
-import Loader from "./Loader";
+import Header from './Header';
+import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   //console.log(navigation);
 
+  //grid with 3 row, first row and last row are 'auto' (size equal content)
+  //second row '1fr' will take up the remaining space
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
