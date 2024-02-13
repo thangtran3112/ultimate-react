220234a1340ffbf768808cb52786824a65beee10
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-12 20:00:18 -0800

    Screensize breakpoints for customizing css

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 function CartOverview() {
   return (
-    <div className="bg-stone-800 uppercase text-stone-200">
-      <p className="text=stone-300 font-semibold">
+    <div className="bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="text=stone-300 space-x-4 font-semibold sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
