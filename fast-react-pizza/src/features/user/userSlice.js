022e84e6b5381e6582644ba79d9cb42079ae0f93
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-27 20:29:11 -0800

    Added user slice and store

--- fast-react-pizza/src/features/user/userSlice.js
+++ fast-react-pizza/src/features/user/userSlice.js
@@ -1,4 +1,4 @@
-import { getAddress } from "../../services/apiGeocoding";
+/*import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
@@ -20,4 +20,23 @@ async function fetchAddress() {
 
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
-}
+}*/
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  username: 'Jonasssss',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+export default userSlice.reducer;
