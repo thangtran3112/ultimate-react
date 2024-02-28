4b9bb736d9dd0548626cf895c3b1bf6a0cdef836
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-28 15:16:50 -0800

    Submitting Order

--- fast-react-pizza/src/features/order/Order.jsx
+++ fast-react-pizza/src/features/order/Order.jsx
@@ -54,7 +54,7 @@ function Order() {
 
       <ul className="border-top divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
