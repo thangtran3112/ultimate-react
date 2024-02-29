193631465c000ca461d677f10644436bee1ce4b7
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-28 20:55:56 -0800

    basic styled components

--- the-wild-oasis/README.md
+++ the-wild-oasis/README.md
@@ -1,3 +1,7 @@
 # Eslint Setup
 
 `npm install --save-dev vite-plugin-eslint eslint-config-react-app eslint`
+
+# VsCode extensions
+
+`vscode-styled-components`
--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,45 @@
+import styled from "styled-components";
+
+//This component will extends all props from <h1> component
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+//This component will extends all props from <button> component
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button>Check In</Button>
+      <Button>Check Out</Button>
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
+  );
 }
 
 export default App;
