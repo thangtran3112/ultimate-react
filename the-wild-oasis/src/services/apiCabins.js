64d617ebd301aef21bd2cd852f58c6270b9dca58
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-03-02 12:43:25 -0800

    createCabin useMutation on React Hook Form

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -12,6 +12,25 @@ export async function getCabins() {
   return data;
 }
 
+export const createCabin = async (newCabin) => {
+  /**
+   * newCabin has the same data model as in Supabase table
+   * Original supabase syntax for inserting a row:
+   * insert([{ some_column: 'someValue', other_column: 'otherValue' }]).select()
+   */
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created");
+  }
+
+  return data;
+};
+
 //check this from APIDoc for cabins
 export const deleteCabin = async (id) => {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
